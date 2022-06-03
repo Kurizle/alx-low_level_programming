1b35ee525bdaae52dc41120dde8d2661477a017a
“Kurai <klgombiro@live.com>
2022-06-02 17:47:52 -0700

    Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
 /*
-* main â€“ starting point of the script
-âˆ**prints the phrase "Programming is like building a multilingual puzzle
-* Return: 0 is the correct value when executed correctly or otherwise
-*/
-int main(void)
+ * main - starting point of the script
+*prints the phrase "Programming is like building a multilingual puzzle
+* Return: 0 if exited properly, non-zero otherwise
+/*
+
+int main(void) 
+
+/* body of main function */
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
