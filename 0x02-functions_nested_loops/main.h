71c034d0efa88a998e320119c536f1b6bed2707a
Kurizle <68846461+Kurizle@users.noreply.github.com>
2022-06-08 03:45:24 +0200

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,12 @@
+#ifndef Main.h
+#define Main.h
+
+/*
+ *Main.h file for the project heare files 
+ *Declerations of all the processes and loop functions
+ *used in the 0x02-functions_nested_loops assignment
+ */
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -11,3 +20,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+
+#endif
