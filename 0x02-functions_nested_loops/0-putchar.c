bed2037d77e1cf87bb2da783e982ddaafd5477e7
“Kurai <klgombiro@live.com>
2022-06-07 19:28:54 -0700

    This file _putchar.c program that prints _putchar, followed by a new line, the program should return 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,20 @@
-#include <stdio.h>
 #include "main.h"
-/**
- *main - start of program
- *Return: on success 1
- *On error, -1 and errno is set appropriately
- */
 
+/**
+ *main - program starts
+ *
+ *Return: alwways 0
+ */
 int main(void)
 {
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n';
+return (0);
+}
