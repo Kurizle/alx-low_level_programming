cbbf435f0b101869fdf8ae07051fc7061b7f9339
“Kurai <klgombiro@live.com>
2022-06-09 20:12:51 -0700

    This file 0-main.c will test that the function positive_or_negative() gives the correct output when given a case of 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
- * main - assigns a random number to int n everytime
- * it executes, and prints it
- * Return: Always 0 (Success)
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
-int positive_or_negative(int n)
-{
-	if (n > 0)
-		printf("%d is positive\n", n);
-	if (n == 0)
-		printf("%d is zero\n", n);
-	if (n < 0)
-		printf("%d is negative\n", n);
-	return (0);
-}
 
+int main(void)
+{
+int i;
+
+i = 0;
+positive_or_negative(i);
+
+return (0);
+}
