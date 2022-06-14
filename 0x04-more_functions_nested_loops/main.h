a4f4d7e56a12db4f30291b2116bf650b100b16c0
“Kurai <klgombiro@live.com>
2022-06-13 18:50:36 -0700

    Header file for the 0x04-more_functions_nested_loops assignment

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,21 @@
+#ifndef _main_h_
+#define _main_h_
+
+/*
+ *main.h file for the project header files 
+ *Declerations of all the processes and loop functions
+ *used in the 0x04-more_functions_nested_loops
+ */
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+int main(void);
+#endif
