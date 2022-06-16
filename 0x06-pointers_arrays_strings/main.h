941ef8ff99a353c6604d476d8510db9f49676e61
“Kurai <klgombiro@live.com>
2022-06-16 12:26:22 -0700

    This file 0-strcat.c concatenates two strings by appending the src string to the dest string, overwriting the terminating null byte (\0) at the end of dest, and then adds a terminating null byte.

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,20 @@
+#ifndef _main_h_
+#define _main_h_
+
+/*
+ *main.h file for the project header files 
+ *Declerations of all the processes and loop functions
+ *used in the 0x06-pointers_arrays_strings assignment
+ */
+
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *rot13(char *);
+
+#endif
