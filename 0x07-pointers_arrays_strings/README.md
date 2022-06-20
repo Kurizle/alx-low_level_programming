f3126f31ab34704a733b304722970d40fc3692a5
“Kurai <klgombiro@live.com>
2022-06-20 12:26:12 -0700

    This file 0-memset.c fills memory with a constant byte.

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Teaches with instruction, about pointers and arrays using the C language; this is the 3rd part of the low level programming course named, 0x07-pointers_arrays_strings.
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,21 @@
+#ifndef _main_h_
+#define _main_h_
+
+/*
+ *main.h file for the project header files 
+ *Declerations of all the processes and loop functions
+ *used in the 0x07-pointers_arrays_strings
+ */
+
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+
+#endif
