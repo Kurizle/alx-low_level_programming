03a56fb88f04a5917f5b4dc6da42afc50d90e64f
“Kurai <klgombiro@live.com>
2022-06-22 06:23:53 -0700

    This file 0-puts_recursion.c prints a string, followed by a new line.

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+Teaches with instruction, about recursions in the C language as part of the low level programming course named, 0x08-recursion.
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,18 @@
+#ifndef _main_h_
+#define _main_h_
+
+/*
+ *main.h file for the project header files 
+ *Declerations of all the processes and loop functions
+ *used in the 0x08-recursion
+ */	
+
+void _puts_recursion(char *s);
+int _putchar(char c);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+
+#endif
